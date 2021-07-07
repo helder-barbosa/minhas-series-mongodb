e2b5dfdf99d33968be5045e971aab4c54fc93d9a
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-07-07 16:08:52 -0300

    create Model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const pages = require('./routes/pages')
+const series = require('./routes/series')
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
 
@@ -17,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use('/', pages)
+app.use('/series', series)
 
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
